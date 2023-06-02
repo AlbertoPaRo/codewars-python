054a4ea63093fb025853ed4dcce1f460b7bca46e
AlbertoPaRo <albertopadillaroj@gmail.com>
2023-06-01 21:45:46 -0400

    add square-n-sum and test

---
+++ square-n-sum/main.py
@@ -0,0 +1,9 @@
+def square_sum(numbers):
+    result = 0
+    for i in numbers:
+        print(i)
+        i = i**2
+        result += i
+        
+    return result
+        
---
+++ square-n-sum/test_main.py
@@ -0,0 +1,7 @@
+from main import square_sum
+
+
+def test_square_sum():
+    entrace = [1,2,2]
+    expect = 9
+    assert square_sum(entrace) == expect
